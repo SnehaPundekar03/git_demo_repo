f374d3f42549b7e562404dc8545de9fa82c97ae0
Pundekar <sneha.pundekar@siemens.com>
2024-09-21 13:58:08 +0530

    Added new feature for ABC Organization in JavaScript

---
+++ Test_Dir_2/feature.js
@@ -0,0 +1,4 @@
+console.log("ABC Organization Feature");
+console.log("Organization Name: ABC Organization");
+console.log("Department: IT Services");
+console.log("Project: Migration to Distributed Version Control");
